ea10003e7cc8467e266fb4f2849f5779e77c88ae
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-11-02 09:39:10 +0100

    navbar now collapseable

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  public navbarCollapsed = true;
+  public components = ["c1","c2","c3"];
 
   ngOnInit() {
   }
