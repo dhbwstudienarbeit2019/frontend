548351701d69f40c684aece8d3d09822b9fb93f7
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-11-02 09:40:10 +0100

    responsive navbar updated demo values

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,22 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { MarkdownComponent } from './markdown/markdown.component';
+import { GraphComponent } from './graph/graph.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainpageComponent,
+    MarkdownComponent,
+    GraphComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule.forRoot(),
-
   ],
   providers: [],
   bootstrap: [AppComponent]
