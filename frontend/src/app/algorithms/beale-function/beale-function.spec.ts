167a427eff88be9a3663112c8b5190b13d7dee18
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-11-07 23:59:05 +0100

    beale test case fix

--- frontend/src/app/algorithms/beale-function/beale-function.spec.ts
+++ frontend/src/app/algorithms/beale-function/beale-function.spec.ts
@@ -3,14 +3,16 @@ import {BealeFunction} from './beale-function';
 
 
 describe('BealeFunction', () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [BealeFunction]
+  }));
 
-    it('should be created', () => {
-        const service: BealeFunction = TestBed.get(BealeFunction);
-        expect(service).toBeTruthy();
-    });
-    it('should be 0 at f(3,0.5)', () => {
-        const service: BealeFunction = TestBed.get(BealeFunction);
-        expect(service.func(3, 0.5)).toBe(0);
-    })
+  it('should be created', () => {
+    const service: BealeFunction = TestBed.get(BealeFunction);
+    expect(service).toBeTruthy();
+  });
+  it('should be 0 at f(3,0.5)', () => {
+    const service: BealeFunction = TestBed.get(BealeFunction);
+    expect(service.func(3, 0.5)).toBeCloseTo(0, 12);
+  });
 });
