80d48ced96124225dbb94eae5eaadc3223aec1d2
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-11-20 23:46:53 +0100

    fixed operator typo

--- frontend/src/app/algorithms/schaffer-n2-function/schaffer-n2-function.ts
+++ frontend/src/app/algorithms/schaffer-n2-function/schaffer-n2-function.ts
@@ -5,10 +5,10 @@ export class SchafferN2Function implements OptimizationAlgorithmInterface {
   public readonly name = 'Schaffer N. 2';
   public readonly func = (x: number, y: number) => {
     const xxyyDif = x * x - y * y;
-    const xxyySum = x * x - y * y;
+    const xxyySum = x * x + y * y;
     const sine = Math.sin(xxyyDif) * Math.sin(xxyyDif);
-    const divident = 1 + 0.001 * (xxyySum);
-    return 0.5 + (sine - 0.5) / (divident * divident);
+    const dividend = 1 + 0.001 * (xxyySum);
+    return 0.5 + (sine - 0.5) / (dividend * dividend);
   };
 
 }
