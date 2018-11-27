2e3c49a48e6b9248b0de04e34fb20e0d310d8910
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-11-27 08:51:30 +0100

    boundaries of bukin function corrected

--- frontend/src/app/algorithms/bukin-function/bukin-function.ts
+++ frontend/src/app/algorithms/bukin-function/bukin-function.ts
@@ -6,7 +6,7 @@ export class BukinFunction implements OptimizationAlgorithmInterface {
     public readonly func = (x: number, y: number) => 100 * Math.sqrt(Math.abs(y - 0.01 * x * x)) + 0.01 * Math.abs(x + 10);
     public readonly searchArea =
         {
-            min: {x: -15, y: -5},
+            min: {x: -15, y: -3},
             max: {x: -5, y: 3}
         };
 }
