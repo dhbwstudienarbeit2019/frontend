d8c0ca4a0bd56aab69f0da5189c66e00ef090633
raspyweather <raspyweather@gmail.com>
2019-03-24 17:14:07 +0100

    algorithms page created

---
+++ frontend/src/app/algorithms-list/algorithms-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-algorithms-list',
+  templateUrl: './algorithms-list.component.html',
+  styleUrls: ['./algorithms-list.component.scss']
+})
+export class AlgorithmsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { FunctionsPageComponent } from './functions-page/functions-page.component';
 import { AlgorithmsPageComponent } from './algorithms-page/algorithms-page.component';
 import { FunctionsListComponent } from './functions-list/functions-list.component';
+import { AlgorithmsListComponent } from './algorithms-list/algorithms-list.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: 'algorithms/:algorithmName/:functionName',
     component: AlgorithmsPageComponent
+  },
+  {
+    path: 'algorithms',
+    component: AlgorithmsListComponent
   }
 ];
 
