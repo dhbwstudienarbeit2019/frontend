72ca144abaeaf37ad0a17b83b7652cf8026a1ce0
raspyweather <raspyweather@gmail.com>
2019-05-16 18:11:46 +0200

    fixed relative markdown image loading

--- frontend/src/app/wiki-entry/wiki-entry.component.ts
+++ frontend/src/app/wiki-entry/wiki-entry.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { RepositoryDetailInformation } from './wikientry.service';
 import { MarkdownComponent, MarkdownService } from 'ngx-markdown';
 import { Renderer } from 'marked';
+import isAbsoluteUrl from 'is-absolute-url';
 
 @Component({
   selector: 'app-wiki-entry',
@@ -30,10 +31,16 @@ export class WikiEntryComponent implements OnInit {
   public set repo(value: RepositoryDetailInformation) {
     this._repo = value;
     console.log(this.repo.markdownUrl.replace(/[^/]*$/, ''));
-    // this.markdownService.compile()
     this.markdownService.renderer = new Renderer({
       baseUrl: this.repo.markdownUrl.replace(/[^/]*$/, ''),
     });
+    const rendererFunction = this.markdownService.renderer.image;
+    this.markdownService.renderer.image = (href, title, text) => {
+      if (!isAbsoluteUrl(href)) {
+        href = this.repo.markdownUrl + '/../' + href;
+      }
+      return rendererFunction.apply(this.markdownService.renderer, [href, title, text]);
+    };
     this.markdownComponent.markdownService = this.markdownService;
   }
   public get repo() {
@@ -41,8 +48,6 @@ export class WikiEntryComponent implements OnInit {
   }
 
   ngOnInit() {
-
-
   }
 
 }
